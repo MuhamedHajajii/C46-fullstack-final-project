f2439d2c682e3e68712740c686f9a42ee44eee5d
Muhamedhajaji <muhamed.hajajii@shezlong.com>
2026-03-17 03:07:52 +0200

    handle ssr

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { BreadCrumbsComponent } from '../../../../shared/components/bread-crumbs/bread-crumbs.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [BreadCrumbsComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
