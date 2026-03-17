2f3836fd6c768ba35e38985626a377e458b1a715
Muhamedhajaji <muhamed.hajajii@shezlong.com>
2026-03-17 05:29:11 +0200

    dist

--- src/app/features/home/components/home-main-slider/home-main-slider.component.ts
+++ src/app/features/home/components/home-main-slider/home-main-slider.component.ts
@@ -1,7 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
-import { Swiper } from 'swiper/types';
 register();
 
 @Component({
@@ -13,13 +12,4 @@ register();
 })
 export class HomeMainSliderComponent {
   spaceBetween = 10;
-
-  onProgress(event: CustomEvent<[Swiper, number]>) {
-    const [swiper, progress] = event.detail;
-    console.log(progress);
-  }
-
-  onSlideChange() {
-    console.log('slide changed');
-  }
 }
